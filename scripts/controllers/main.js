9a83f09470aa2e8ed48c64a67afee95b8c35a0f1
thscm <lingang.yang@gmail.com>
2015-01-20 02:12:17 +0800

    layout 1-20

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -8,22 +8,67 @@
  * Controller of the vtoneWorldcomApp
  */
 angular.module('vtoneWorldcomApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
 
 angular.module('vtoneWorldcomApp')
-  .controller('MenuLeftCtrl', function ($scope) {
-	$scope.MenuLeftData = [
-		{"descr":"首页","href":"#","class":"active"},
-		{"descr":"手工建单","href":"#","class":""},
-		{"descr":"订单记录","href":"#","class":"dropdown","submenu":[{"descr":"Action"},{"descr":"Other Action"}]},
-		{"descr":"零售商品","href":"#","class":"dropdown","submenu":[{"descr":"Action"},{"descr":"Other Action"}]},
-		{"descr":"订单审核","href":"#","class":"dropdown","submenu":[{"descr":"Action"},{"descr":"Other Action"}]}
-		
-	];
-  });
+    .controller('MenuLeftCtrl', function($scope) {
+        $scope.MenuLeftData = [{
+                "descr": "首页",
+                "href": "#",
+                "class": "active"
+            }, {
+                "descr": "手工建单",
+                "href": "#",
+                "class": ""
+            }, {
+                "descr": "订单记录",
+                "href": "#",
+                "class": "dropdown",
+                "submenu": [{
+                    "descr": "Action"
+                }, {
+                    "descr": "Other Action"
+                }]
+            }, {
+                "descr": "零售商品",
+                "href": "#",
+                "class": "dropdown",
+                "submenu": [{
+                    "descr": "Action"
+                }, {
+                    "descr": "Other Action"
+                }]
+            }, {
+                "descr": "订单审核",
+                "href": "#",
+                "class": "dropdown",
+                "submenu": [{
+                    "descr": "Action"
+                }, {
+                    "descr": "Other Action"
+                }]
+            }
+
+        ];
+    });
+
+function layout() {
+    $("#main").css("width", $(window).width() - 200);
+    $("#sidebar").css("height", $(window).height() - 45);
+    $("#main").css("height", $(window).height() - 45);
+}
+jQuery(function($) {
+    $(document).ready(function() {
+        layout();
+    });
+});
+
+$(window).resize(function() {
+    layout();
+});
