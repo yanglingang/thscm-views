3f81b24b61651f8acb8bc4860df0529e2c29bfbc
yanglingang <yanglingang@gmail.com>
2015-01-21 14:48:43 +0800

    layout 1-21 3

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -15,46 +15,40 @@ angular.module('vtoneWorldcomApp')
             'Karma'
         ];
     });
-
+/*jshint unused: false */
 angular.module('vtoneWorldcomApp').controller('TopMenuCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('scripts/menu.json').
     success(function(data, status, headers, config) {
         $scope.TopMenuData = data;
         $scope.$watch('TopMenuData', function() {
-            setTimeout(function(){
-                $(".TopMenu").dropdown();
-            },1000);
+            setTimeout(function() {
+                $('.TopMenu').dropdown();
+            }, 1000);
         });
     }).
     error(function(data, status, headers, config) {
-        alert(status);
+        return status;
     });
 }]);
-
+// Stuff
 
 function layout() {
-    $('#main').css('width', $(window).width() - 240);
-    $('#main').css('height', $(window).height() - 82);
-    $('#main_carouse').css('height', $(window).height() - 92);
-}
-
+        $('#main').css('width', $(window).width() - 240);
+        $('#main').css('height', $(window).height() - 82);
+        $('#main_carouse').css('height', $(window).height() - 92);
+    }
+    /*jshint unused: false */
 function moveMainCarouse(index) {
-    $('#main_carouse').find(".markers").find("a").eq(index).trigger('click');
-}
+        $('#main_carouse').find('.markers').find('a').eq(index).trigger('click');
+    }
+    // Stuff
 $(function($) {
     $(document).ready(function() {
         layout();
-        $('#main_carouse').find(".markers").css("display", "none");
+        //$('#main_carouse').find('.markers').css('display', 'none');
     });
 });
 
 $(window).resize(function() {
     layout();
-});
-$('#main_carouse').carousel({
-    auto: false,
-    stop: true,
-    markers: {
-        type: "square"
-    }
 });
